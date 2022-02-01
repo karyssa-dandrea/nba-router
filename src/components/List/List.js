4dc24c5206bbad5478b38d2a8f5761f45df15a14
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-01 12:25:27 -0800

    got my villagers to render

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export default function List() {
-  return <div></div>;
+export default function List({ villagers }) {
+  return (
+    <div>
+      <h1>Animal Crossing Cuties:</h1>
+      {villagers.map((vill) => (
+        <>
+          <p key={vill.id}>{vill.name['name-USen']}</p>
+          <img src={vill.icon_uri} />
+        </>
+      ))}
+    </div>
+  );
 }
 
 // rendering info for each villager
