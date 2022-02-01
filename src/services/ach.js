35008d6cc3fed41f326cac1f5cca63594e3d9fd2
karyssa-dandrea <karyssadandrea@gmail.com>
2022-01-31 17:26:43 -0800

    fixed my functions

--- src/services/ach.js
+++ src/services/ach.js
@@ -1,10 +1,10 @@
 export async function getVillager() {
-  const resp = await fetch('https://acnhapi.com/v1a/');
+  const resp = await fetch('http://acnhapi.com/v1a/villagers');
   const data = await resp.json();
   return data;
 }
 
-export async function getVillagerByID(id) {
+export async function getVillagerByID(villagerID) {
   const resp = await fetch(`http://acnhapi.com/v1a/villagers/${villagerID}`);
   const data = await resp.json();
   return data;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Home() {
+  const [villagers, setVillagers] = useState({});
+
   return <div></div>;
 }
