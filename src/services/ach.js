b8d15839b2d2150840c55faa3aca6fbe81d02821
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-02 12:45:00 -0800

    added https

--- src/services/ach.js
+++ src/services/ach.js
@@ -1,11 +1,11 @@
 export async function getVillager() {
-  const resp = await fetch('http://acnhapi.com/v1a/villagers');
+  const resp = await fetch('https://acnhapi.com/v1a/villagers');
   const data = await resp.json();
   return data;
 }
 
 export async function getVillagerByID(villagerID) {
-  const resp = await fetch(`http://acnhapi.com/v1a/villagers/${villagerID}`);
+  const resp = await fetch(`https://acnhapi.com/v1a/villagers/${villagerID}`);
   const data = await resp.json();
   return data;
 }
