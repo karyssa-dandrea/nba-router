dadc935a9197d1a137bc0f3009438dd5c4a30f37
karyssa-dandrea <karyssadandrea@gmail.com>
2022-02-01 16:53:34 -0800

    added a usehistory

--- src/views/Home/ACH/AchDetail.js
+++ src/views/Home/ACH/AchDetail.js
@@ -4,11 +4,13 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Details from '../../../components/List/Details/Details';
 import { getVillagerByID } from '../../../services/ach';
+import { useHistory } from 'react-router-dom';
 
 export default function AchDetail() {
   const [villager, setVillager] = useState([]);
   const { villagerID } = useParams();
   const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,9 +25,15 @@ export default function AchDetail() {
     return <h1> Loading...</h1>;
   }
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    history.push('/');
+  };
+
   return (
     <div>
       <Details villager={villager} />
+      <button onClick={handleClick}>Previous Page</button>
     </div>
   );
 }
